ec0285095c3f0e4125db6c1111407609d9a76706
MThomas564 <38511675+MThomas564@users.noreply.github.com>
2021-01-10 18:48:11 +0000

    first commit

---
+++ src/App/Program.cs
@@ -0,0 +1,29 @@
+﻿using Fclp;
+using System;
+
+namespace FHIRValidate.App {
+    class Program {
+        public static CommandArgs Params { get; set; }
+
+        static void Main(string[ ] args) {
+            // Console.WriteLine("Hello World!");
+            // var p = new FluentCommandLineParser<CommandArgs>();
+
+            // p.Setup(arg => arg.Directory)
+            //     .As('d', "Directory")
+            //     .Required();
+
+            // Params = p.Object;
+            // Params.Directory = "/Users/mthomas/Development/FHIRValidate/TestData";
+
+            FHIRValidate.Dev.Validate v = new Dev.Validate("/Users/mthomas/Development/FHIRValidate/TestData");
+
+            v.Execute();
+        }
+
+    }
+
+    public class CommandArgs {
+        public string Directory { get; set; }
+    }
+}
---
+++ src/Dev/Validate.cs
@@ -0,0 +1,65 @@
+﻿using EnsureThat;
+using Hl7.Fhir.Model;
+using Hl7.Fhir.Serialization;
+using System;
+using System.IO;
+using System.Linq;
+
+namespace FHIRValidate.Dev {
+    public class Validate {
+        private readonly string _directory;
+        public Validate(string Directory) {
+            Ensure.That(Directory).IsNotNull();
+            _directory = Directory;
+        }
+
+        public void Execute() {
+            //Load files
+            var files = LoadFiles(_directory);
+            foreach (var f in files) {
+                var type = Path.GetExtension(f);
+                var content = File.ReadAllText(f);
+
+                if (type == ".json") {
+                    var @return = LoadJson(content);
+                    // _resourceParser.Parse()
+                }
+                if (type == ".xml") {
+                    var @return = LoadXml(content);
+                }
+
+
+            }
+        }
+
+        protected string[ ] LoadFiles(string strDir) {
+            return Directory.EnumerateFiles(strDir, "*.*", SearchOption.AllDirectories).Where(s => s.EndsWith(".xml") || s.EndsWith(".json")).ToArray();
+        }
+
+        //loadJson
+        protected Hl7.Fhir.Model.Base LoadJson(string s) {
+            var parser = new FhirJsonParser();
+
+            try {
+                var parsed = parser.Parse(s);
+                return parsed;
+            } catch (FormatException fe) {
+                throw fe;
+            }
+        }
+
+        //loadXml
+        protected Hl7.Fhir.Model.Base LoadXml(string s) {
+            var parser = new FhirXmlParser();
+
+            try {
+                var parsed = parser.Parse(s);
+                return parsed;
+            } catch (FormatException fe) {
+                // the boring stuff
+                throw fe;
+            }
+
+        }
+    }
+}
